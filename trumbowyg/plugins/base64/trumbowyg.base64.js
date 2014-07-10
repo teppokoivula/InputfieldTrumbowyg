178bbdaa10b31b3d5b9d4dd46da994ee32b46c28
teppokoivula <teppo@piikkilanka.com>
2014-07-10 08:02:39 +0300

    Add base64 plugin

---
+++ trumbowyg/plugins/base64/trumbowyg.base64.js
@@ -0,0 +1,72 @@
+/* ===========================================================
+ * trumbowyg.base64.js
+ * Base64 plugin for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : Cyril Biencourt (lizardK)
+ */
+
+(function($){
+    $.extend(true, $.trumbowyg, {
+        langs: {
+            en: {
+                base64: "Image as base64",
+                file:   "File",
+                errFileReaderNotSupported: "FileReader is not supported by your browser."
+            },
+            fr: {
+                base64: "Image en base64",
+                file:   "Fichier"
+            }
+        },
+
+        opts: {
+            btnsDef: {
+                base64: {
+                    isSupported: function(){
+                        if(typeof FileReader === "undefined"){
+                            console.err('[Trumbowyg - Plugin base64] FileReader is not supported by your browser.');
+                            return false;
+                        }
+                        return true;
+                    },
+                    func: function(params, tbw){
+                        var file,
+                            $modal = tbw.openModalInsert(
+                            // Title
+                            tbw.lang['base64'],
+
+                            // Fields
+                            {
+                                file: {
+                                    type: 'file',
+                                    required: true
+                                },
+                                alt: {
+                                    label: 'description'
+                                }
+                            },
+
+                            // Callback
+                            function(values, fields){
+                                var data = new FormData(),
+                                fReader  = new FileReader();
+
+                                fReader.onloadend = function () {
+                                    tbw.execCommand('insertImage', fReader.result);
+                                    tbw.closeModal();
+                                }
+
+                                fReader.readAsDataURL(file);
+                            }
+                        );
+
+                        $('input[type=file]').on('change', function(e){
+                            file = e.target.files[0];
+                        });
+                    }
+                }
+            }
+        }
+    });
+})(jQuery);
